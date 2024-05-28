f2826a09898d4429420c0bcb448541041c4665e2
Dmitry <chepikov.d.u@gmail.com>
2024-05-28 19:16:52 +0300

    git initialization

---
+++ src/main/java/ru/chepikov/linkshortener/LinkShortenerApp.java
@@ -0,0 +1,10 @@
+package ru.chepikov.linkshortener;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

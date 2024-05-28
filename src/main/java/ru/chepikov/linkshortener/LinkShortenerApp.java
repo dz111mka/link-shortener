75fa0e4e5f84e35ac6830c52b16ccab8ea67799b
Dmitry <chepikov.d.u@gmail.com>
2024-05-28 22:04:11 +0300

    small fix

--- src/main/java/ru/chepikov/linkshortener/LinkShortenerApp.java
+++ src/main/java/ru/chepikov/linkshortener/LinkShortenerApp.java
@@ -1,10 +1,11 @@
 package ru.chepikov.linkshortener;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LinkShortenerApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(LinkShortenerApp.class, args);
     }
 }

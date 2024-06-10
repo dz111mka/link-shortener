ea91c016ae51658acb9283b218a9feded217a5b1
Dmitry <chepikov.d.u@gmail.com>
2024-06-11 00:21:49 +0300

    small fix

--- src/main/java/ru/chepikov/linkshortener/LinkShortenerApp.java
+++ src/main/java/ru/chepikov/linkshortener/LinkShortenerApp.java
@@ -2,21 +2,16 @@ package ru.chepikov.linkshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.chepikov.linkshortener.dto.CreateShortLinkRequest;
-import ru.chepikov.linkshortener.service.LinkInfoService;
-
-import java.time.ZonedDateTime;
+import ru.chepikov.LoggingConfiguration;
+import ru.chepikov.LoggingConfigurationGradle;
 
 @SpringBootApplication
 public class LinkShortenerApp {
     public static void main(String[] args) {
         SpringApplication.run(LinkShortenerApp.class, args);
 
-        LinkInfoService linkService = new LinkInfoService();
+        LoggingConfiguration.testLog("Hi to all");
+        LoggingConfigurationGradle.testLog("test");
 
-        CreateShortLinkRequest shortLinkRequest = new CreateShortLinkRequest("google.com", ZonedDateTime.now(), "desc", true);
-
-        System.out.println(linkService.getByShortLink(
-                linkService.createLinkInfo(shortLinkRequest).getShortLink()));
     }
 }
